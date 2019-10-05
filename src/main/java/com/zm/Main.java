b6503dc7be6785e036d01c27a31554aacf933a6f
zm <1368643161@qq.com>
2019-10-05 21:57:35 +0800

    第一次提交

---
+++ src/main/java/com/zm/Main.java
@@ -0,0 +1,11 @@
+package com.zm;
+
+/**
+ * @author zm
+ * @date 2019/10/5 21:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
